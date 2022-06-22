8b8aafc358491f780dac9c8221c2d6d1f0d6744f
kimdila <90942034+kimdila@users.noreply.github.com>
2022-06-22 10:46:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+//VoiceRSS Javascript SDK
+
+//Disable/Enable Button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+//Passing Joke to VoiceRSS API
+function tellMe(joke) {
+  VoiceRSS.speech({
+    key: "340c8141d10046ab90253d49481eef3f",
+    src: joke,
+    hl: "en-us",
+    v: "Linda",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+
+// Get jokes from API
+async function getJokes() {
+  let joke = "";
+  const apiUrl =
+    "https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    //Text-to-Speech
+    tellMe(joke);
+    //Disable Button
+    toggleButton();
+  } catch (error) {
+    console.log("whoops", error);
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
